a4bbc544b3ebb5a62f71c391b64c469450268813
yourname <liujiannan@imudges.com>
2016-07-26 16:33:03 +0800

    登录

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from flask.ext.cors import CORS
 app = Flask(__name__)
 CORS(app)
 from app import views
-from app import views2
+#from app import views2
--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
 from app import app
-from flask import Flask
-
+from flask import Flask,jsonify
+from flask import request,render_template,url_for
+from db import User
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "HELLO,LOVEU!"
+    return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    form = request.form
+    userphone = form.get('UserPhone')
+    PassWord = form.get('PassWord')
+    print userphone
+    print PassWord
+    if not userphone:
+        return '0'
+    else:
+        if not PassWord:
+            return '2'
+        else:
+            u=User.query.filter_by(UserPhone=userphone).first()
+            if u.PassWord == PassWord:
+
+                return '1'
+            else:
+                return '3'
+
+
+
+
