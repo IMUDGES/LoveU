e7369868c778e78a1fe32665190c4a18eb954b00
heshulin//gitcafe <2287373451@qq.com>
2016-07-26 15:55:57 +0800

    db模块补充

--- app/db.py
+++ app/db.py
@@ -12,27 +12,98 @@ manager = Manager(app)
 
 class User(db.Model):
     UserId = db.Column(db.Integer, primary_key=True)
-    UserPhone = db.Column(db.Integer, unique=True)
-    PassWord = db.Column(db.String(120), unique=True)
-    NickName = db.Column(db.String(120), unique=True)
-    TrueName = db.Column(db.String(120), unique=True)
+    UserPhone = db.Column(db.String, unique=True)
+    PassWord = db.Column(db.String, unique=True)
+    NickName = db.Column(db.String, unique=True)
+    TrueName = db.Column(db.String, unique=True)
     UserSex = db.Column(db.Integer, unique=True)
-    UserGrade = db.Column(db.String(120), unique=True)
-    UserPhoto = db.Column(db.String(120), unique=True)
-    SecretKey = db.Column(db.String(120), unique=True)
-    UserMajor = db.Column(db.String(120), unique=True)
+    UserGrade = db.Column(db.String, unique=True)
+    UserPhoto = db.Column(db.String, unique=True)
+    SecretKey = db.Column(db.String, unique=True)
+    UserMajor = db.Column(db.String, unique=True)
 
     def __repr__(self):
         return '<User %r>' % self.UserPhone
 
 
 class Checkcode(db.Model):
-    CheckId = db.Column(db.String(120), primary_key=True)
-    UserPhone = db.Column(db.String(120), unique=True)
-    CheckCode = db.Column(db.String(120), unique=True)
+    CheckId = db.Column(db.Integer, primary_key=True)
+    UserPhone = db.Column(db.String, unique=True)
+    CheckCode = db.Column(db.Integer, unique=True)
 
     def __repr__(self):
         return '<User %r>' % self.UserPhone
+
+
+class Class(db.Model):
+    ClassId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, unique=True)
+    Day = db.Column(db.Integer, unique=True)
+    Number = db.Column(db.Integer, unique=True)
+    Information = db.Column(db.String(120), unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.UserId
+
+
+class Config(db.Model):
+    ConfigId = db.Column(db.Integer, primary_key=True)
+    Version = db.Column(db.Integer, unique=True)
+    Url = db.Column(db.String(120), unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.ConfigId
+
+
+class Food(db.Model):
+    FoodId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, unique=True)
+    FoodArea = db.Column(db.String(120), unique=True)
+    FoodInformation = db.Column(db.String(120), unique=True)
+    GetUser = db.Column(db.String(120), unique=True)
+    FoodTime = db.Column(db.DateTime, unique=True)
+    FoodWay = db.Column(db.String, unique=True)
+    State = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.UserId
+
+
+class Give(db.Model):
+    GiveId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, unique=True)
+    GiveInformatinon = db.Column(db.String, unique=True)
+    GiveImage = db.Column(db.String, unique=True)
+    GetUser = db.Column(db.Integer, unique=True)
+    State = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.UserId
+
+
+class GiveComment(db.Model):
+    CommentId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, unique=True)
+    CommentInformation = db.Column(db.String, unique=True)
+    GiveId = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.UserId
+
+
+class Help(db.Model):
+    HelpId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, unique=True)
+    HelpMoney = db.Column(db.Integer, unique=True)
+    DownTime = db.Column(db.DateTime, unique=True)
+    HelpInformation = db.Column(db.String, unique=True)
+    GetUser = db.Column(db.Integer, unique=True)
+    State = db.Column(db.Integer, unique=True)
+    Finish = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.UserId
+
 
 if __name__ == '__main__':
     manager.run()
