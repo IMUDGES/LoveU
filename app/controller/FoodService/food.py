631fb74e278522eeb8ee5769e763f3a595c116c9
yourname <liujiannan@imudges.com>
2016-07-27 16:08:37 +0800

    版本升级

--- app/controller/FoodService/food.py
+++ app/controller/FoodService/food.py
@@ -2,12 +2,28 @@
 from app.bean.secretkey import Secretkey
 from app.db import User
 from flask import request
+from app.db import Food
 
 
 class foodservice():
     def creat(self):
-        Secretkey = request.args.get('SecretKey')
-        UserPhone = request.args.get()
-        if not Secretkey:
+        self.Secretkey = request.args.get('SecretKey')
+        self.UserPhone = request.args.get()
+        if not self.UserPhone or not self.Secretkey:
+            msg = '请登录'
+        else:
+            u = User.query.filter_by(UserPhone = self.UserPhone).first()
+            if u.SecretKey != Secretkey:
+                msg = '请登录'
+            else:
+                f = Food
+                f.UserId = u.UserId
+                f.FoodArea = request.args.get('FoodArea')
+                f.FoodInformation = request.args.get('FoodInformation')
+                f.FoodTime = request.args.get()
+
+
+
+
 
 
