fb2315b8aa56fb792c2ae3f669d79169cc3d397d
heshulin//gitcafe <2287373451@qq.com>
2016-07-29 17:46:39 +0800

    七牛云存储

--- app/bean/qiniuup.py
+++ app/bean/qiniuup.py
@@ -1,13 +1,16 @@
 # -*- coding:utf-8 -*-
 from qiniu import Auth, put_file, etag, urlsafe_base64_encode
 import qiniu.config
+from app.bean.usetphotorandom import Userphotorandom
 
 #此模块未完成
 class Qiniuup(object):
     def up(self):
         q = Auth("gFO-8IYwjVPzNAmbAORHJCgGwIHzcyIbFhZ3yVIi", "hllClWcBETkcn0aI8SROEe4Y1blV5gEQwgUHAQQu")
         bucket_name = 'loveu'
-        key = '图片名字';
+        userphotorandom = Userphotorandom()
+        #图片名字
+        key = userphotorandom.getuserphotorandom()
         token = q.upload_token(bucket_name, key)
         localfile = '本地图片路径'
         ret, info = put_file(token, key, localfile)
---
+++ app/bean/usetphotorandom.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import string, random, hashlib
+
+
+class Userphotorandom():
+    def getuserphotorandom(self):
+        str2=random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a','1','2','3','4','5','6','7','8','9','0'], 36)
+        str3 = ''
+        for i in str2:
+            str3 = str3 + i
+        str4 = str3.encode('utf-8')
+        m = hashlib.md5()
+        m.update(str4)
+        psw = m.hexdigest()
+        return psw
