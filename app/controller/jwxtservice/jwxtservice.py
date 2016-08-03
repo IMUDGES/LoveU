63ccd8875080b1f71c01f02278705c9e25131b75
heshulin//gitcafe <2287373451@qq.com>
2016-08-03 10:43:17 +0800

    教务系统选课模块完成

--- app/controller/jwxtservice/jwxtservice.py
+++ app/controller/jwxtservice/jwxtservice.py
@@ -5,6 +5,41 @@ import json
 
 
 class JwxtService(object):
+    # 教务系统账号，教务系统密码，课程号，课序号
+    def chooseClass(self, JwxtNumber, JwxtPassword, ClassNumber, ClassOrder):
+        if ClassOrder < 10:
+            ClassOrder = '0' + str(ClassOrder)
+        else:
+            ClassOrder = str(ClassOrder)
+        h = httplib2.Http()
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Connection": "keep-alive",
+            "Host": "jwxt.imu.edu.cn",
+            "Referer": "http://jwxt.imu.edu.cn/logout.do",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+        }
+        resp, content = h.request("http://jwxt.imu.edu.cn/loginAction.do?zjh=" + JwxtNumber + "&mm=" + JwxtPassword,
+                                  "GET", headers=headers)
+        print(resp)
+        cookie = resp['set-cookie'].replace('; path=/', "")
+        headers['Cookie'] = cookie
+        resp2, content2 = h.request("http://jwxt.imu.edu.cn/xkAction.do?actionType=6", "GET", headers=headers)
+        resp3, content3 = h.request("http://jwxt.imu.edu.cn/xkAction.do?actionType=-1&fajhh=32874", "GET",
+                                    headers=headers)
+        resp4, content4 = h.request("http://jwxt.imu.edu.cn/xkAction.do?actionType=2&pageNumber=-1&oper1=ori", "GET",
+                                    headers=headers)
+        resp5, content5 = h.request(
+            "http://jwxt.imu.edu.cn/xkAction.do?jhxn=&kcsxdm=&kch=" + ClassNumber + "&cxkxh=&actionType=2&oper2=gl&pageNumber=-1",
+            "GET", headers=headers)
+        resp6, content6 = h.request(
+            "http://jwxt.imu.edu.cn/xkAction.do?kcId=" + ClassNumber + "_" + ClassOrder + "&preActionType=2&actionType=9",
+            "GET", headers=headers)
+        print(content6.decode('gb2312'))
+        # print(resp)
+
     def loginService(self, jwxtnumber, jwxtpassword):
         url = "http://183.175.14.250:8080/JwxtInterface/check.html?zjh=" + jwxtnumber + "&mm=" + jwxtpassword
         conn = httplib2.Http()
